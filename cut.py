8c33b3005d4870943f7a4cac2c5507f389696f41
Tom Viner <tom.viner@sohonet.com>
2018-12-06 21:03:36 +0000

    parse fields

--- cut.py
+++ cut.py
@@ -1,6 +1,27 @@
+from itertools import count
+
 import click
 
 from backend import doCut, Mode
+
+
+def get_fields(ranges):
+    spans = []
+    for rng in ranges:
+        start, dash, stop = rng.partition('-')
+        start = int(start or 1)
+        stop = int(stop or 0)
+        spans.append((start, dash, stop))
+
+    for start, dash, stop in sorted(spans):
+        # print(start, dash, stop)
+        if dash:
+            if not stop:
+                yield from count(start)
+            else:
+                yield from range(start, stop + 1)
+        else:
+            yield start
 
 
 @click.command()
@@ -11,22 +32,15 @@ def cut(input, d, f):
     # print(input, d)
     # print(input.read())
     ranges = f.split(',')
-    fields = set()
-    for rng in ranges:
-        start, dash, stop = rng.partition('-')
-        # print(start, dash, stop)
-        if dash:
-            if stop:
-                stop = int(stop) + 1
-            else:
-                stop = None
-            fs = range(int(start) or 1, stop)
-            fields.update(fs)
-            # print(fs)
-        else:
-            fields.add(int(start))
+    # fields = set()
+
     # print(sorted(fields))
-    doCut(f=input, mode=Mode.DELIM, delim=d, fields=sorted(fields))
+    # for i, fld in enumerate(get_fields(ranges)):
+    #     if i > 10:
+    #         break
+    #     print(fld)
+
+    doCut(f=input, mode=Mode.DELIM, delim=d, fields=get_fields(ranges))
 
 
 if __name__ == '__main__':
