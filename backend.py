932e7834fff990e085da3f8e3b49c77914e1649b
Abigail Brady <abigail.brady@filigree.tech>
2018-12-06 21:06:26 +0000

    backend.py

--- backend.py
+++ backend.py
@@ -7,30 +7,52 @@ class Mode(enum.Enum):
 
 def doCut(f, mode: Mode, delim: str, fields: typing.Iterator[int]) -> None:
     
+    seen = []
+
+    def isIn(index):
+        if index in seen:
+            return True
+
+        for f in fields:
+            seen.append(f)
+            if f == index:
+                return True
+            if f >= index:
+                return False
+
+        return False
+
     for line in f:
 
+        line = line.rstrip('\n')
+
         if mode == Mode.CHAR:
+            var = line
+            delim = ""
 
-            newline = ""
-
-            for field in fields:
-                if field >= len(line):
-                    break
-                newline += line[field - 1]
-
-    
         elif mode == Mode.DELIM:
-
             var = line.split(delim)
 
-            newfields = [field for idx, field in enumerate(var) if idx + 1 in fields]
+        newfields = [field for idx, field in enumerate(var) if isIn(idx + 1)]
 
-            newline = delim.join(newfields)
+        newline = delim.join(newfields)
 
         print(newline)
 
 
 
+def yielder():
+    yield 1
+    n = 2 
+    while True:
+        n += 1
+        yield n
 
 
-    
+
+#for mode in [Mode.CHAR, Mode.DELIM]:
+#    for fields in [yielder(), [1, 3, 4]]:
+#
+#        with open("test.csv", "r") as f:
+#            doCut(f, mode, ",", fields)
+        
